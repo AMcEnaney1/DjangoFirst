6ecbfeab429835e7ae057349f21300bb7a1427cb
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-06-05 13:18:27 -0400

    admin stuff and blog thingy

--- personal_portfolio/projects/admin.py
+++ personal_portfolio/projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from projects.models import Project
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project)
+from django.contrib import admin
 
 # Register your models here.
