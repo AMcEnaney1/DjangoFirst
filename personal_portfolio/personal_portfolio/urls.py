77107af3874ad3423ccc2aeb83060bda011e2e72
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-06-05 22:23:10 -0400

    got homepage up
    Now have homepage, still need to allow access to resume and add css for all other pages.

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("projects.urls")),
+    path('', include("home.urls")),
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
