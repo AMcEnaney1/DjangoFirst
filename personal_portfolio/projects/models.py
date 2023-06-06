9a76072bc46dc397c4c4a7949400500305dfaa5e
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-06-06 10:55:00 -0400

    super janky very very bad way to get image paths working for both admin and shell

--- personal_portfolio/projects/models.py
+++ personal_portfolio/projects/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path= os.path.join(os.getcwd(), "projects", "static", "img")) # Janky surely, but works
+    technology = models.CharField(max_length=100)
+    #image = models.FilePathField(path= os.path.join(os.getcwd(), "projects", "static", "img")) # Janky surely, but works
+    image = models.FilePathField(path = "projects/static/img")
