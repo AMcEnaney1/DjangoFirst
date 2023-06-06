f5b419f6d0b4b37dc64e86052d3681cf18885129
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-06-06 14:23:08 -0400

    added login and logout functionality, as well as the ability for a new user to sign up

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
